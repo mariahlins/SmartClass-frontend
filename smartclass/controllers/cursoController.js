a28c226d7b3a7defa82596ea870a5fd56cbea5a2
Gustavo Santos <alexandregustavo00@gmail.com>
2025-04-14 11:54:18 -0300

    fix: ajusta as urls

--- smartclass/controllers/cursoController.js
+++ smartclass/controllers/cursoController.js
@@ -3,7 +3,7 @@ import api from '../services/api';
 class CursoController{
     static async listar(){
         try{
-            const response = await api.get('lms/list-cursos/');
+            const response = await api.get('lms/cursos/');
             return response.data;
         }catch(error){
             console.error('Erro ao listar cursos:', error.response ? error.response.data : error.message);
